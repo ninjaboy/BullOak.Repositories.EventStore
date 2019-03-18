57db90c047baa94b38695c9194d9b106431b6770
Savvas Kleanthous <skleanthous@outlook.com>
2019-03-18 22:21:54 +0000

    Moved from BO. Initial

---
+++ src/BullOak.Repositories.EventStore.Test.Integration/Components/IMyEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace BullOak.Repositories.EventStore.Test.Integration.Components
+{
+    using System;
+
+    public interface IMyEvent
+    {
+        Guid Id { get; set; }
+        int Value { get; set; }
+    }
+}
---
+++ src/BullOak.Repositories.EventStore.Test.Integration/Components/MyEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace BullOak.Repositories.EventStore.Test.Integration.Components
+{
+    using System;
+
+    public class MyEvent : IMyEvent
+    {
+        public Guid Id { get; set; }
+        public int Value { get; set; }
+
+        public MyEvent(int value)
+        {
+            Id = Guid.NewGuid();
+            Value = value;
+        }
+    }
+}
